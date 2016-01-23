aadbe2e5c8f04074e701615ed71476cf971ee9d0
misfitphill <phill@misfit.com>
2016-01-24 00:44:50 +0800

    added control register map values

--- AD5933.h
+++ AD5933.h
@@ -8,53 +8,60 @@
 
 #define AD5933_ADR 0x0D;
 
-//register map
+//register map (data sheet p23)
 #define REG_CTRL0 0x80
 #define REG_CTRL1 0x81
-
 #define REG_START_FREQ0 0x82
 #define REG_START_FREQ1 0x83
 #define REG_START_FREQ2 0x84
-
 #define REG_FREQ_INC0 0x85
 #define REG_FREQ_INC1 0x86
 #define REG_FREQ_INC2 0x87
-
 #define REG_NUM_INCS0 0x88
 #define REG_NUM_INCS1 0x89
-
 #define REG_SETTLE0 0x8A
 #define REG_SETTLE1 0x8B
-
 #define REG_STATUS 0x8F
-
 #define REG_TEMP0 0x92
 #define REG_TEMP1 0x93
-
 #define REG_REAL0 0x94
 #define REG_REAL1 0x95
-
 #define REG_IMG0 0x96
 #define REG_IMG1 0x97
+
+//control register map (data sheet p23)
+const int INIT_START_FREQ = 0x10;
+const int START_FREQ_SWEEP = 0x20;
+const int INC_FREQ = 0x30;
+const int REPEAT_FREQ = 0x40;
+const int MEASURE_TEMP = 0x90;
+const int POWER_DOWN = 0xA0;
+const int STANDBY = 0xB0;
+const int RANGE_1 = 0x00;
+const int RANGE_2 = 0x06;
+const int RANGE_3 = 0x04;
+const int RANGE_4 = 0x02;
 
 class AD5933{
     public:
     	//temperature
-    	bool readTemp();
+    	double readTemp();
 
     	//start frequency
     	bool readStartFreq();
-    	bool writeStartFreq(long);
+    	bool writeStartFreq(long freq);
 
     	//frequency increment
     	bool readFreqInc();
-    	bool writeFreqInc(long);
+    	bool writeFreqInc(long freq);
 
     	//number of frequency increments
     	bool readNumFreqInc();
-    	bool writeNumFreqInc(int);
+    	bool writeNumFreqInc(int num);
 
     	//settling time cycles
     	bool readSettlingTimeCycles();
     	bool writeSettlingTimeCycles(int);
 }
+
+#endif
