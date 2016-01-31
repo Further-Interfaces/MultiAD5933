c0a8c74d53675110d8487e13e79e5651a4ee1699
furtherphill <phillip.pasqual@gmail.com>
2016-02-01 00:15:32 +0800

    added functions to reset the device, set an external clock and read the temperature

--- MultiAD5933.cpp
+++ MultiAD5933.cpp
@@ -5,8 +5,63 @@ AD5933::AD5933(){
 
 }
 
+/*
+Reads the current temperature of the device and
+returns the value in celcius
+*/
 double AD5933::readTemp(){
+	//send temp measure commmand to control register
+	if(setCtrlRegister(MEASURE_TEMP)){
+		//wait for temp measure to complete
+		while(!isMeasurementComplete(VALID_TEMP_MEASURE)){
+			;
+		}
 
+		int temp0 = getByteFromAddr(REG_TEMP0);
+		int temp1 = getByteFromAddr(REG_TEMP1);
+
+		//convert to celcius
+		int maxBits = 14;
+		int total = 0;
+		int idx = 0;
+		while(idx < maxBits){
+			if(idx < 8){
+				if(temp0 & 1){
+					total += pow(2,idx);
+				}
+				temp0 >>= 1;
+			}else{
+				if(temp1 & 1){
+					total += pow(2,idx);
+				}
+				temp1 >>= 1;
+			}
+			idx++;
+		}
+		return (double)total/32.0;
+	}
+}
+
+/*
+Reset the AD5933
+*/
+bool AD5933::reset(){
+	int addr = REG_CTRL1;
+	int ctrlByte = getByteFromAddr(addr);
+	ctrlByte &= 0x04; //keep D3 only
+	ctrlByte |= RESET; //set D4 (reset)
+	return setByteToAddr(addr, ctrlByte);
+}
+
+/*
+Set if an external system clock is being used
+*/
+bool AD5933::setExtSysClock(bool usingExt){
+	if(usingExt){
+		return setByteToAddr(REG_CTRL1, EXT_SYS_CLK);
+	}else{
+		return setByteToAddr(REG_CTRL1, 0x00);
+	}
 }
 
 
@@ -17,17 +72,19 @@ reading temperature, power down, etc.
 See page 23 of data sheet.
 */
 bool AD5933::setCtrlRegister(int mode){
-	int ctrlReg = REG_CTRL0;
-	if(mode == RESET || mode == EXT_SYS_CLK){
-		ctrlReg = REG_CTRL1;
-		if(mode == RESET)
-			mode = 0x10;
-	}
-
-	int ctrlByte = getByteFromAddr(ctrlReg);
+	int ctrlByte = getByteFromAddr(REG_CTRL0);
+	ctrlByte &= 0x0F; //only keep D8-D11
 	ctrlByte |= mode;
 
-	bool success = setByteToAddr(ctrlReg, ctrlByte);
+	bool success = setByteToAddr(REG_CTRL0, ctrlByte);
+
+	#if LOG_ENABLED
+		if(!success){
+			Serial.print("ERROR Setting Ctrl Reg: ");
+			Serial.println(ctrlByte, HEX);
+		}
+	#endif
+
 	return success;
 }
 
@@ -39,11 +96,33 @@ See page 28 of data sheet
 */
 bool AD5933::setAddressRegister(int addr){
 	Wire.beginTransmission(AD5933_ADDR);
-	//write addr to the address register
+	//write addr to the address pointer register
 	Wire.write(ADDR_POINTER);
 	Wire.write(addr);
 
 	Wire.endTransmission();
+}
+
+/*
+Polls the status register to check if a certain
+measurement (mode) is complete. 
+*/
+bool AD5933::isMeasurementComplete(int mode){
+	int ctrlByte = getByteFromAddr(REG_CTRL0);
+	Serial.println(ctrlByte,HEX);
+	//get byte from status register
+	int status = getByteFromAddr(REG_STATUS);
+
+	if(mode == VALID_TEMP_MEASURE || mode == VALID_DATA || mode == VALID_FREQ_SWEEP){
+		//Serial.print(status,HEX);
+		Serial.println();
+		return status & mode == mode;
+	}else{
+		#if LOG_ENABLED
+			Serial.println("ERROR invalid mode for polling status register");
+		#endif
+		return false;
+	}
 }
 
 /*
@@ -73,7 +152,18 @@ bool AD5933::setByteToAddr(int addr, int val){
 	Wire.beginTransmission(AD5933_ADDR);
 	Wire.write(addr);
 	Wire.write(val);
-	Wire.endTransmission();
+	int i2cReturn = Wire.endTransmission();
+	Serial.println("i2cReturn: " + i2cReturn);
+	if(i2cReturn == 0){
+		return true;
+	}else{
+		#if LOG_ENABLED
+			Serial.print("ERROR Setting Byte: ");
+			Serial.print(addr, HEX);
+			Serial.println(val, HEX);
+		#endif
+		return false;
+	}
 }
 
 
--- MultiAD5933.h
+++ MultiAD5933.h
@@ -3,6 +3,7 @@
 
 #include<Wire.h> // I2C
 #include<Arduino.h> 
+#include <math.h>
 
 #define LOG_ENABLED 1
 
@@ -10,25 +11,25 @@
 #define ADDR_POINTER 0xB0
 
 //register map (data sheet p23)
-#define REG_CTRL0 0x80
-#define REG_CTRL1 0x81
-#define REG_START_FREQ0 0x82
-#define REG_START_FREQ1 0x83
-#define REG_START_FREQ2 0x84
-#define REG_FREQ_INC0 0x85
-#define REG_FREQ_INC1 0x86
-#define REG_FREQ_INC2 0x87
-#define REG_NUM_INCS0 0x88
-#define REG_NUM_INCS1 0x89
-#define REG_SETTLE0 0x8A
-#define REG_SETTLE1 0x8B
-#define REG_STATUS 0x8F
-#define REG_TEMP0 0x92
-#define REG_TEMP1 0x93
-#define REG_REAL0 0x94
-#define REG_REAL1 0x95
-#define REG_IMG0 0x96
-#define REG_IMG1 0x97
+const int REG_CTRL0 = 0x80;
+const int REG_CTRL1 = 0x81;
+const int REG_START_FREQ0 = 0x82;
+const int REG_START_FREQ1 = 0x83;
+const int REG_START_FREQ2 = 0x84;
+const int REG_FREQ_INC0 = 0x85;
+const int REG_FREQ_INC1 = 0x86;
+const int REG_FREQ_INC2 = 0x87;
+const int REG_NUM_INCS0 = 0x88;
+const int REG_NUM_INCS1 = 0x89;
+const int REG_SETTLE0 = 0x8A;
+const int REG_SETTLE1 = 0x8B;
+const int REG_STATUS = 0x8F;
+const int REG_TEMP0 = 0x93;
+const int REG_TEMP1 = 0x92;
+const int REG_REAL0 = 0x94;
+const int REG_REAL1 = 0x95;
+const int REG_IMG0 = 0x96;
+const int REG_IMG1 = 0x97;
 
 //control register map (data sheet p23)
 const int INIT_START_FREQ = 0x10;
@@ -42,8 +43,14 @@ const int RANGE_1 = 0x00;
 const int RANGE_2 = 0x06;
 const int RANGE_3 = 0x04;
 const int RANGE_4 = 0x02;
-const int RESET = 0x01; //actual value should be 0x10 (already taken)
+
+const int RESET = 0x10; 
 const int EXT_SYS_CLK = 0x08;
+
+//status register polling
+const int VALID_TEMP_MEASURE = 0x01;
+const int VALID_DATA = 0x02;
+const int VALID_FREQ_SWEEP = 0x04;
 
 class AD5933{
     public:
@@ -67,13 +74,19 @@ class AD5933{
     	//bool readSettlingTimeCycles();
     	//bool writeSettlingTimeCycles(int);
 
+        bool setExtSysClock(bool usingExt);
+        bool reset();
+
     private:
     	int getByteFromAddr(int addr);
     	bool setByteToAddr(int addr, int val);
 
         bool setAddressRegister(int addr);
-    	
     	bool setCtrlRegister(int mode);
+
+        bool isMeasurementComplete(int mode);
+
+
 };
 
 #endif
